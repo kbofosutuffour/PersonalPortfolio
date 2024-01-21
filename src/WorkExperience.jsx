03c672e48f85c70db3cbd4e7a4ba527003a6af64
Kwaku Ofosu <kbofosutuffour@gmail.com>
2024-01-21 14:29:42 -0500

    few changes

--- src/WorkExperience.jsx
+++ src/WorkExperience.jsx
@@ -2,7 +2,7 @@ import './App.css';
 
 export default function WorkExperience() {
     return (
-        <div id="workexperience" className='border border-dark p-5 m-md-5 mt-5 d-flex flex-column justify-content-center align-items-center'>
+        <div id="workexperience" className='border border-dark p-5 m-md-5 mt-5 d-flex flex-column justify-content-center align-items-center rounded'>
             <h3><u>Work Experience</u></h3>
             <div className='column row-md p-md-5 d-flex flex-column-reverse flex-md-row justify-content-center align-items-center'>
                 {/* <div className='col-md'>
@@ -10,11 +10,13 @@ export default function WorkExperience() {
                     </div>
                 </div> */}
                 <div className='col-md-9'>
-                    <h6>Chesterfield County IST: Data Analytics Intern</h6>
+                    <h5>Chesterfield County IST: Data Analytics Intern</h5>
                     <span>
-                    Responsible for managing and analyzing data commonly used by various departments of 
-                    Chesterfield County’s Department of Human Resources.  Queried data from SQL databases using Microsoft Azure Studio.
-                    Developed queries to determine inconsistencies across different database schema
+                        Here, I worked closely with the Data & Analytics team of Chesterfield County IST.  I was responsible for managing and
+                        analyzing data commonly used by various departments throughout Chesterfield County&#39;s department of Human Resources.  The data analysis was
+                        done using SQL, specifically MySQL using Microsoft Azure Studio.  The queries developed were used to determine inconsistencies that were
+                        present across different database schema, such as differing addresses or differing descriptions for county buildings.  These inconsistencies were
+                        mainly found by relating rows of tables using a building&#39;s Parcel ID.
                     </span>
                 </div>
             </div>
